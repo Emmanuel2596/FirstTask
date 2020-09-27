a09ece23ad5b6ce95d754b9a95422f9cc0434509
Emmanuel2596 <emmanuel.arguedas@ulead.ac.cr>
2020-09-26 19:21:49 -0600

    Tarea de Progra

---
+++ src/VariableExamples.java
@@ -0,0 +1,14 @@
+public class VariableExamples {
+
+    public static void main(String[] args){
+
+short lowNumber = 32767;
+long highNumber = 17179869184; // No idea why Is it marking integer number too large if it's a long.
+double decimalNumber = 29.5d // This has been LDA's number of champion cups for the last 7 years.
+
+
+
+    }
+
+
+}
